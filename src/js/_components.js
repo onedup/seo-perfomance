f208d116235adb04fd990e8dbd785832e6e0b97c
onedup <onedup>
2024-08-16 14:10:28 +0300

    Update 2024-08-16T11:10:18.937Z

--- src/js/_components.js
+++
@@ -1,5 +0,0 @@
-import "./components/header-fix.js"
-import "./components/hero-bg-animation.js"
-import "./components/services-swiper.js"
-import "./components/spoiler.js"
-import "./components/companies-sliders.js"
